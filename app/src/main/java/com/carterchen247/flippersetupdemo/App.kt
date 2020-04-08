708f8f160819d6b38cf460d63e2f9555c6c1cb72
CarterChen <carter.chen@langlive.com>
2020-04-08 21:54:56 +0800

    NetworkFlipperPlugin is added

--- app/src/main/java/com/carterchen247/flippersetupdemo/App.kt
+++ app/src/main/java/com/carterchen247/flippersetupdemo/App.kt
@@ -6,6 +6,7 @@ import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import com.facebook.soloader.SoLoader
 
 class App : Application() {
@@ -17,6 +18,7 @@ class App : Application() {
         if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
             val client = AndroidFlipperClient.getInstance(this)
             client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(NetworkFlipperPlugin())
             client.start()
         }
     }
